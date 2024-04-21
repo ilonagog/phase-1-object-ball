cf56afcd5a6d4af1e641964a7e92320800087187
ilona <gogiashvili.ilona@gmail.com>
2024-04-21 15:18:58 -0400

    commit tree c7b36948e1c59f0bd499751f902250a114228461

---
+++ src/00-objectball.js
@@ -0,0 +1,11 @@
+function gameObject() {
+  const obj = {
+    home: {
+      teamName: "Brooklyn Nets",
+      color: [Black, White],
+      players: {},
+    },
+  };
+  console.log(obj);
+}
+gameObject();
